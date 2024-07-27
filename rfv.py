eb11f66ee8c041ee4358c66fc406b4eaa0f7d448
Pedro Lucas Azevedo Hang <hangpedroo@hotmail.com>
2024-07-27 09:31:23 -0300

    New

---
+++ rfv.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+st.set_page_config(
+     page_title="RFV Customer Segmentation Template",
+     page_icon="   https://cdn-icons-png.flaticon.com/512/17436/17436453.png "
+)
